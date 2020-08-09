6172e30493d48d42664a26b8f280f49b9ecf6064
David Mario <davidmariolc.dev@gmail.com>
2020-08-09 14:40:39 -0500

    Updates

---
+++ precache-manifest.9d1603b911d07d434ceedcd8618e7970.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2eda0d1433a58fdf551679ffaf616f30",
+    "url": "/calculador-costos/index.html"
+  },
+  {
+    "revision": "75874be754c389151cd6",
+    "url": "/calculador-costos/static/css/main.25ab6ca1.chunk.css"
+  },
+  {
+    "revision": "fab4c0b11a5208d02f6e",
+    "url": "/calculador-costos/static/js/2.96b31ef6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculador-costos/static/js/2.96b31ef6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75874be754c389151cd6",
+    "url": "/calculador-costos/static/js/main.ffd7f31c.chunk.js"
+  },
+  {
+    "revision": "a15ba1fcd51f81651167",
+    "url": "/calculador-costos/static/js/runtime-main.0fabff3a.js"
+  }
+]);
